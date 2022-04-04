977b3137073f356eb522f2ca0ec0969d7bafd295
Micka73100 <Michael-73@live.Fr>
2022-04-04 16:47:32 +0200

    router

--- app/Controllers/BlogController.php
+++ app/Controllers/BlogController.php
@@ -1,6 +1,20 @@
 <?php
 
+namespace App\Controllers;
 
+
+class BlogController {
+
+    public function index()
+    {
+        echo 'je suis la homepage';
+    }
+
+    public function show(int $id)
+    {
+        echo 'Je suis le post' . $id;
+    }
+}
 
 
 
---
+++ routes/Route.php
@@ -0,0 +1,52 @@
+<?php
+
+namespace Router;
+
+
+
+class Route {
+
+
+    public $path;
+    public $action;
+    public $matches;
+
+    public function __construct($path, $action)
+    {
+        $this->path= trim($path, '/');
+        $this->action= $action;
+    }
+
+    public function matches(string $url)
+    {
+        $path = preg_replace('#:([\w]+)#', '([^/]+)', $this->path);
+        $pathToMatch = "#^$path$";
+
+        if (preg_match($pathToMatch, $url, $matches)){
+        $this->matches = $matches;
+        return true;
+        }else {
+            return false;
+        }
+    }
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
