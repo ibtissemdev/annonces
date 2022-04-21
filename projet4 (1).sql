d3baa0cd2a137177f108f8695a2ab95d030e9db3
ibtissemdev <ibtissem.khiri@gmail.comgit config --global user.email ibtissem.khiri@gmail.com>
2022-04-21 15:09:46 +0200

    bdd

--- projet4 (1).sql
+++ projet4 (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Hôte : localhost:3306
--- Généré le : mar. 12 avr. 2022 à 07:03
+-- Généré le : jeu. 21 avr. 2022 à 13:08
 -- Version du serveur : 5.7.33
 -- Version de PHP : 7.4.27
 
@@ -46,8 +46,12 @@ CREATE TABLE `annonces` (
 --
 
 INSERT INTO `annonces` (`id`, `categorie`, `nom`, `description`, `prix`, `ville`, `photo1`, `photo2`, `photo3`, `photo4`, `photo5`) VALUES
-(51, 'loisirs', 'Eric Martin', 'fbdfchbdhb', 23, 'Grenoble', '', NULL, NULL, NULL, NULL),
-(52, 'loisirs', 'Eric Martin', 'fbdfchbdhb', 23, 'Grenoble', '', NULL, NULL, NULL, NULL);
+(175, 'immobilier', 'modification', 'hkgkgkg', 22, 'Chambery', NULL, NULL, NULL, NULL, NULL),
+(176, 'immobilier', 'modification', 'hkgkgkg', 22, 'Chambery', NULL, NULL, NULL, NULL, NULL),
+(177, 'vehicule', 'deId Test', 'fbdfchbdhb', 30, 'Grenoble', 'http://localhost/annonces/public/images/bananas-282313_1920.jpg', 'http://localhost/annonces/public/images/', 'http://localhost/annonces/public/images/', 'http://localhost/annonces/public/images/', 'http://localhost/annonces/public/images/'),
+(178, 'vehicule', 'onteste', 'hkgkgkg', 22, 'Grenoble', NULL, NULL, NULL, NULL, NULL),
+(180, 'loisirs', 'modification parmail', 'fbdfchbdhb', 23, 'Grenoble', 'http://localhost/annonces/public/images/doughnuts-1868573_1920.jpg', 'http://localhost/annonces/public/images/', 'http://localhost/annonces/public/images/', 'http://localhost/annonces/public/images/', 'http://localhost/annonces/public/images/'),
+(181, 'vehicule', 'Alice Durand', 'gsgsdg', 22, 'Grenoble', 'http://localhost/annonces/public/images/', 'http://localhost/annonces/public/images/', 'http://localhost/annonces/public/images/', 'http://localhost/annonces/public/images/', 'http://localhost/annonces/public/images/');
 
 -- --------------------------------------------------------
 
@@ -57,6 +61,7 @@ INSERT INTO `annonces` (`id`, `categorie`, `nom`, `description`, `prix`, `ville`
 
 CREATE TABLE `utilisateurs` (
   `id` int(10) UNSIGNED NOT NULL,
+  `id_annonce` int(30) DEFAULT NULL,
   `mail` varchar(40) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 
@@ -64,67 +69,15 @@ CREATE TABLE `utilisateurs` (
 -- Déchargement des données de la table `utilisateurs`
 --
 
-INSERT INTO `utilisateurs` (`id`, `mail`) VALUES
-(100, 'semsem73@hotmail.fr'),
-(101, 'semsem73@hotmail.fr'),
-(102, 'semsem73@hotmail.fr'),
-(103, 'semsem73@hotmail.fr'),
-(104, 'semsem73@hotmail.fr'),
-(105, 'semsem73@hotmail.fr'),
-(106, 'semsem73@hotmail.fr'),
-(107, 'semsem73@hotmail.fr'),
-(108, 'semsem73@hotmail.fr'),
-(109, 'semsem73@hotmail.fr'),
-(110, 'test@test.fr'),
-(111, 'test@test.fr'),
-(112, 'mail@gmail.fr'),
-(113, 'mail@gmail.fr'),
-(114, 'mail@gmail.fr'),
-(115, 'mail@gmail.fr'),
-(116, 'mail@gmail.fr'),
-(117, 'mail@gmail.fr'),
-(118, 'mail@gmail.fr'),
-(119, 'gesgsg.simplon@gmail.com'),
-(120, 'semsem@hotmail.fr'),
-(121, 'semsem@hotmail.fr'),
-(122, 'geg@gmail.com'),
-(123, 'geg@gmail.com'),
-(124, 'ibtissemkhiri.simplon@gmail.com'),
-(125, 'ibtissemkhiri.simplon@gmail.com'),
-(126, 'ibtissemkhiri.simplon@gmail.com'),
-(127, 'ibtissemkhiri.simplon@gmail.com'),
-(128, 'ibtissemkhiri.simplon@gmail.com'),
-(129, 'ibtissemkhiri.simplon@gmail.com'),
-(130, 'ibtissemkhiri.simplon@gmail.com'),
-(131, 'ibtissemkhiri.simplon@gmail.com'),
-(132, 'semsem73@hotmail.fr'),
-(133, 'semsem73@hotmail.fr'),
-(134, 'semsem73@hotmail.fr'),
-(135, 'gsdg@gmail.com'),
-(136, 'gsdg@gmail.com'),
-(137, 'vfsf@geg.gt'),
-(138, 'vfsf@geg.gt'),
-(139, 'vfsf@geg.gt'),
-(140, 'vfsf@geg.gt'),
-(141, 'vfsf@geg.gt'),
-(142, 'vfsf@geg.gt'),
-(143, 'semsem73@hotmail.fr'),
-(144, 'semsem73@hotmail.fr'),
-(145, 'ibtissemkhiri.simplon@gmail.com'),
-(146, 'semsem73@hotmail.fr'),
-(147, 'semsem73@hotmail.fr'),
-(148, 'ibtissemkhiri.simplon@gmail.com'),
-(149, 'semsem73@hotmail.fr'),
-(150, 'mail@gmail.com'),
-(151, 'semsem73@hotmail.fr'),
-(152, 'semsem73@hotmail.fr'),
-(153, 'semsem73@hotmail.fr'),
-(154, 'semsem73@hotmail.fr'),
-(155, 'semsem73@hotmail.fr'),
-(156, 'mail@gmail.com'),
-(157, 'semsem73@hotmail.fr'),
-(158, 'mail@gmail.com'),
-(159, 'mail@gmail.com');
+INSERT INTO `utilisateurs` (`id`, `id_annonce`, `mail`) VALUES
+(278, 175, 'ibtissemkhiri.simplon@gmail.com'),
+(279, 176, 'ibtissemkhiri.simplon@gmail.com'),
+(280, 177, 'ibtissemkhiri.simplon@gmail.com'),
+(281, 178, 'ibtissemkhiri.simplon@gmail.com'),
+(282, 179, 'ibtissemkhiri.simplon@gmail.com'),
+(283, 180, 'ibtissemkhiri.simplon@gmail.com'),
+(284, 181, 'ibtissemkhiri.simplon@gmail.com'),
+(285, 182, 'ibtissemkhiri.simplon@gmail.com');
 
 --
 -- Index pour les tables déchargées
@@ -150,13 +103,13 @@ ALTER TABLE `utilisateurs`
 -- AUTO_INCREMENT pour la table `annonces`
 --
 ALTER TABLE `annonces`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=53;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=183;
 
 --
 -- AUTO_INCREMENT pour la table `utilisateurs`
 --
 ALTER TABLE `utilisateurs`
-  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=160;
+  MODIFY `id` int(10) UNSIGNED NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=286;
 COMMIT;
 
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
