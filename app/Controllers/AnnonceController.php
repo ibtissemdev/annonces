0ab8ef66f9552268ad41865624e55a722c3d7a88
ibtissemdev <khiri@nicomak.eu>
2022-10-22 21:39:08 +0200

    essaie uplaod

--- app/Controllers/AnnonceController.php
+++ app/Controllers/AnnonceController.php
@@ -66,35 +66,35 @@ class AnnonceController extends Controller
         // error_log(print_r($result, 1));
 
         //On leur supprime le path pour ne laisser que le nom du fichier photo et on les stocke dans un tableau
-        // $photoAsupprimer = [];
-        // foreach ($result as $photoPourSup) {
-        //     $photoPourSup = str_replace(self::PATH_IMG_ABSOLUTE, "", $photoPourSup->chemin);
+        $photoAsupprimer = [];
+        foreach ($result as $photoPourSup) {
+            $photoPourSup = str_replace(self::PATH_IMG_ABSOLUTE, "", $photoPourSup->chemin);
 
-        //     $photoAsupprimer[] = $photoPourSup;
-        // }
+            $photoAsupprimer[] = $photoPourSup;
+        }
 
-        // // error_log(print_r($photoAsupprimer, 1));
-        // //On parcour le dossier image et on stocke chaque fichier image dans un tableau
-        // $dir = "images/";
-        // $dossier = opendir($dir);
-        // $fichierImage = [];
-        // while ($fichier = readdir($dossier)) {
-        //     if ($fichier != '.' && $fichier != '..') {
-        //         // error_log(print_r($fichier, 1));
-        //         $fichierImage[] = $fichier;
-        //     }
-        // }
-        // foreach ($photoAsupprimer as $aSupprimer) {
-        //     foreach ($fichierImage as $fich) {
-        //         if ($aSupprimer == $fich) {
-        //             // error_log(print_r("fichier dans table photo : " . $aSupprimer, 1));
-        //             // error_log(print_r("fichier dans dossier image : " . $fich, 1));
-        //             unlink("images/" . $fich);
-        //         }
-        //     }
-        // }
+        // error_log(print_r($photoAsupprimer, 1));
+        //On parcour le dossier image et on stocke chaque fichier image dans un tableau
+        $dir = "images/";
+        $dossier = opendir($dir);
+        $fichierImage = [];
+        while ($fichier = readdir($dossier)) {
+            if ($fichier != '.' && $fichier != '..') {
+                // error_log(print_r($fichier, 1));
+                $fichierImage[] = $fichier;
+            }
+        }
+        foreach ($photoAsupprimer as $aSupprimer) {
+            foreach ($fichierImage as $fich) {
+                if ($aSupprimer == $fich) {
+                    // error_log(print_r("fichier dans table photo : " . $aSupprimer, 1));
+                    // error_log(print_r("fichier dans dossier image : " . $fich, 1));
+                    unlink("images/" . $fich);
+                }
+            }
+        }
 
-        // closedir($dossier);
+        closedir($dossier);
 
         foreach ($result as $photoSup) {
             $photo->delete('id_photo', $photoSup->id_photo);
@@ -144,36 +144,36 @@ class AnnonceController extends Controller
             $maxSize = 5000000;
 
             $nbrPhoto = count($_FILES);
-error_log(print_r($_FILES,1));
+            error_log(print_r($_FILES, 1));
 
             for ($i = 0; $i <= $nbrPhoto; $i++) {
                 $filename = $_FILES["file"]['name'][$i];
 
                 $location = './images/' . $filename;
-                // $file_extension = pathinfo($location, PATHINFO_EXTENSION);
-                // $file_extension = strtolower($file_extension);
+                $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+                $file_extension = strtolower($file_extension);
 
-                // // for ($i = 1; $i <= 5; $i++) {
+                // for ($i = 1; $i <= 5; $i++) {
 
-                // //     $filename = $_FILES["photo$i"]['name'];
-                // //     $location = './images/' . $filename;
-                // //     $file_extension = pathinfo($location, PATHINFO_EXTENSION);
-                // //     $file_extension = strtolower($file_extension);
+                //     $filename = $_FILES["photo$i"]['name'];
+                //     $location = './images/' . $filename;
+                //     $file_extension = pathinfo($location, PATHINFO_EXTENSION);
+                //     $file_extension = strtolower($file_extension);
 
-                // if (in_array($file_extension, $valid_ext)) {
-                //     if ($_FILES["file"]['size'][$i] < $maxSize) {
+                if (in_array($file_extension, $valid_ext)) {
+                    if ($_FILES["file"]['size'][$i] < $maxSize) {
 
-                //         // $photo[$i + 1] = $filename;
-                //         // error_log(print_r($_FILES["file"]['size'][$i], 1));
-                       
-                //     } else {
-                //         echo 'taille trop grande ! ';
-                //         // error_log(print_r($_FILES["file"]['size'][$i], 1));
-                //     }
-                // } else {
-                //     echo 'extension non valide ! ';
-                //     // error_log('extension non valide');
-                // }
+                        // $photo[$i + 1] = $filename;
+                        // error_log(print_r($_FILES["file"]['size'][$i], 1));
+
+                    } else {
+                        echo 'taille trop grande ! ';
+                        // error_log(print_r($_FILES["file"]['size'][$i], 1));
+                    }
+                } else {
+                    echo 'extension non valide ! ';
+                    // error_log('extension non valide');
+                }
                 move_uploaded_file($_FILES["file"]['tmp_name'][$i], $location);
             }
 
@@ -186,24 +186,15 @@ error_log(print_r($_FILES,1));
                 ->setDescription($_POST['description'])
                 ->setPrix($_POST['prix'])
                 ->setVille($_POST['ville'])
-                    ->setCategorie_id($_POST['categorie']);
-                // ->setphoto1($_FILES['photo1']['name'])
-                // ->setphoto2($_FILES['photo2']['name'])
-                // ->setphoto3($_FILES['photo3']['name'])
-                // ->setphoto4($_FILES['photo4']['name'])
-                // ->setphoto5($_FILES['photo5']['name']);
-            
+                ->setCategorie_id($_POST['categorie']);
+
+
 
 
             //CONDITION POUR VERIFIER SI ON A UN ID ALORS ON APPELLE LA FONCTION UPDATE
             if (isset($_POST['id']) && !empty($_POST['id'])) {
                 //Upload des images qui vont remplacer celles d'origine
 
-                // for ($i = 1; $i <= 5; $i++) {
-                //     $filename = $_FILES["photo$i"]['name'];
-                //     $photo[$i + 1] = $filename;
-                //     move_uploaded_file($_FILES["photo$i"]['tmp_name'], './images/' . $filename);
-                // }
 
                 //Update des images
                 $valid_ext = array("gif", "jpg", "png", "jpeg", "webp", "jfif");
@@ -218,23 +209,17 @@ error_log(print_r($_FILES,1));
                     $file_extension = pathinfo($location, PATHINFO_EXTENSION);
                     $file_extension = strtolower($file_extension);
 
-                    // for ($i = 1; $i <= 5; $i++) {
-
-                    //     $filename = $_FILES["photo$i"]['name'];
-                    //     $location = './images/' . $filename;
-                    //     $file_extension = pathinfo($location, PATHINFO_EXTENSION);
-                    //     $file_extension = strtolower($file_extension);
 
                     if (in_array($file_extension, $valid_ext)) {
                         // if ($_FILES["file"]['size'][$i] < $maxSize) {
 
-                            // $photo[$i + 1] = $filename;
-                            // error_log(print_r($_FILES["file"]['size'][$i], 1));
-                           
-                            move_uploaded_file($_FILES["file"]['tmp_name'][$i], $location);
+                        // $photo[$i + 1] = $filename;
+                        // error_log(print_r($_FILES["file"]['size'][$i], 1));
+
+                        move_uploaded_file($_FILES["file"]['tmp_name'][$i], $location);
                         // } else {
-                            // echo 'taille trop grande  ';
-                            // error_log(print_r($_FILES["file"]['size'][$i], 1));
+                        // echo 'taille trop grande  ';
+                        // error_log(print_r($_FILES["file"]['size'][$i], 1));
                         // }
                     } else {
                         echo 'extension non valide';
@@ -310,26 +295,21 @@ error_log(print_r($_FILES,1));
             //element constituants les donnees récupérés dans l'Url       0 => idTmp       
             if (!empty($donnees[0]) && ($_COOKIE['idTmp'] == $donnees[0])) {
                 $newAnnonce = $annonce
-                    ->setVille($donnees[1])  //1=>ville
-                    ->setCategorie_id($donnees[2])                              
+                    ->setVille($donnees[1])                             //1=>ville
+                    ->setCategorie_id($donnees[2])                      //2=>id_categorie
                     ->setNom($donnees[3])                               //3=>nom
                     ->setPrix($donnees[4])                              //4=>prix
-                    ->setDescription($donnees[5])                       //5=>description
-                    // lien de la photo en absolu
-                    // ->setphoto1(self::PATH_IMG_ABSOLUTE . $donnees[7])    //7=>photo1
-                    // ->setphoto2(self::PATH_IMG_ABSOLUTE . $donnees[8])    //8=>photo8
-                    // ->setphoto3(self::PATH_IMG_ABSOLUTE . $donnees[9])    //9=>photo9
-                    // ->setphoto4(self::PATH_IMG_ABSOLUTE . $donnees[10])   //10=>photo10
-                    // ->setphoto5(self::PATH_IMG_ABSOLUTE . $donnees[11])  //11=>photo11
-                  ;                    
+                    ->setDescription($donnees[5]);                      //5=>description
+
+
 
                 //Insère l'annonce une fois que l'utilisateur a valider avec l'e-mail
                 $result = $annonce->insert($newAnnonce);
                 //print_r($newAnnonce);
                 //Insertion de l'e-mail dans la table mail avec l'id_annonce
                 $newMail = $mail->setMail($donnees[6])->setId_annonce($result);
-              $compteur=count($donnees)-2;
-error_log(print_r("nombre de données : ".count($donnees),1));
+                $compteur = count($donnees) - 2;
+                error_log(print_r("nombre de données : " . count($donnees), 1));
                 // Insertion des photos dans la table photo
                 for ($i = 7; $i < $compteur; $i++) {
                     $newPoto = $photo->setChemin(self::PATH_IMG_ABSOLUTE . $donnees[$i]);
@@ -377,42 +357,42 @@ error_log(print_r("nombre de données : ".count($donnees),1));
                 }
             } else {
                 echo 'Le temps imparti est écoulé';
-                // $photo = new Photo($this->getDb());
+                $photo = new Photo($this->getDb());
 
-                // //On récupère la liste des photos à supprimer dans la table photos
-                // $result = $photo->findAllChemin();
-                // // error_log(print_r($result, 1));
+                //On récupère la liste des photos à supprimer dans la table photos
+                $result = $photo->findAllChemin();
+                // error_log(print_r($result, 1));
 
-                // //On leur supprime le path pour ne laisser que le nom du fichier photo et on les stocke dans un tableau
-                // $photoAsupprimer = [];
-                // foreach ($result as $photoPourSup) {
-                //     $photoPourSup = str_replace(self::PATH_IMG_ABSOLUTE, "", $photoPourSup->chemin);
+                //On leur supprime le path pour ne laisser que le nom du fichier photo et on les stocke dans un tableau
+                $photoAsupprimer = [];
+                foreach ($result as $photoPourSup) {
+                    $photoPourSup = str_replace(self::PATH_IMG_ABSOLUTE, "", $photoPourSup->chemin);
 
-                //     $photoAsupprimer[] = $photoPourSup;
-                // }
+                    $photoAsupprimer[] = $photoPourSup;
+                }
 
-                // // error_log(print_r($photoAsupprimer, 1));
-                // //On parcour le dossier image et on stocke chaque fichier image dans un tableau
-                // $dir = "images/";
-                // $dossier = opendir($dir);
-                // $fichierImage = [];
-                // while ($fichier = readdir($dossier)) {
-                //     if ($fichier != '.' && $fichier != '..') {
-                //         // error_log(print_r($fichier, 1));
-                //         $fichierImage[] = $fichier;
-                //     }
-                // }
+                // error_log(print_r($photoAsupprimer, 1));
+                //On parcour le dossier image et on stocke chaque fichier image dans un tableau
+                $dir = "images/";
+                $dossier = opendir($dir);
+                $fichierImage = [];
+                while ($fichier = readdir($dossier)) {
+                    if ($fichier != '.' && $fichier != '..') {
+                        // error_log(print_r($fichier, 1));
+                        $fichierImage[] = $fichier;
+                    }
+                }
 
-                // $imagesAsupprimer = array_diff($fichierImage, $photoAsupprimer);
+                $imagesAsupprimer = array_diff($fichierImage, $photoAsupprimer);
 
-                // foreach ($imagesAsupprimer as $aSupprimer) {
+                foreach ($imagesAsupprimer as $aSupprimer) {
 
-                //     // error_log(print_r("fichier dans table photo : " . $aSupprimer, 1));
-                //     // error_log(print_r("fichier dans dossier image : " . $fich, 1));
-                //     unlink("images/" . $aSupprimer);
-                //     // error_log(print_r("images dans dossier image supprimées", 1));
-                // }
-                // closedir($dossier);
+                    // error_log(print_r("fichier dans table photo : " . $aSupprimer, 1));
+                    // error_log(print_r("fichier dans dossier image : " . $fich, 1));
+                    unlink("images/" . $aSupprimer);
+                    // error_log(print_r("images dans dossier image supprimées", 1));
+                }
+                closedir($dossier);
 
 
                 header('Location: /annonces/?cookie=ecoule');
@@ -448,12 +428,12 @@ error_log(print_r("nombre de données : ".count($donnees),1));
                 ->setPrix($_POST['prix'])
                 ->setVille($_POST['ville'])
                 ->setCategorie_id($_POST['categorie']);
-                // ->setphoto1(self::PATH_IMG_ABSOLUTE . $_FILES['file']['name'])
-                // ->setphoto2(self::PATH_IMG_ABSOLUTE . $_FILES['file']['name'])
-                // ->setphoto3(self::PATH_IMG_ABSOLUTE . $_FILES['file']['name'])
-                // ->setphoto4(self::PATH_IMG_ABSOLUTE . $_FILES['file']['name'])
-                // ->setphoto5(self::PATH_IMG_ABSOLUTE . $_FILES['file']['name'])
-              
+            // ->setphoto1(self::PATH_IMG_ABSOLUTE . $_FILES['file']['name'])
+            // ->setphoto2(self::PATH_IMG_ABSOLUTE . $_FILES['file']['name'])
+            // ->setphoto3(self::PATH_IMG_ABSOLUTE . $_FILES['file']['name'])
+            // ->setphoto4(self::PATH_IMG_ABSOLUTE . $_FILES['file']['name'])
+            // ->setphoto5(self::PATH_IMG_ABSOLUTE . $_FILES['file']['name'])
+
 
             //CONDITION POUR VERIFIER SI ON A UN ID ALORS ON APPELLE LA FONCTION UPDATE
             if (isset($_POST['id']) && !empty($_POST['id'])) {
